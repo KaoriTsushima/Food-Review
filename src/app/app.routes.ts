f8e2d2afc432de13df59e2f5c609cc2bd1a74200
Wai Get Law <wai.law@sky.uk>
2025-05-24 21:01:56 +0100

    adding initial pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { OfficeBreakfastComponent } from './components/office-breakfast/office-breakfast.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'office-breakfast', component: OfficeBreakfastComponent },
+];
