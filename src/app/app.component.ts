5abbcf67f80ea0943dc370a00785080e972038b0
Kaori Tsushima <kaoritsushima0107@gmail.com>
2025-05-25 16:27:26 +0100

    installed ng Zorro and created home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { filter, Subscription } from 'rxjs';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzLayoutModule, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  headerVisible: boolean = false;
+  routerSub: Subscription;
+  constructor(private readonly router: Router) {
+    this.routerSub = router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        this.headerVisible = event.url !== '/';
+      });
+  }
   title = 'food-review';
+
+  // todo ngDestroy remove subscription
 }
---
+++ src/app/components/office-lunch/office-lunch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-office-lunch',
+  standalone: true,
+  imports: [],
+  templateUrl: './office-lunch.component.html',
+  styleUrl: './office-lunch.component.css'
+})
+export class OfficeLunchComponent {
+
+}
